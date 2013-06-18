586ff61e724764e205b9a67aa92a78b09774a19b
Ben Bates <ben.bates@runkeeper.com>
2013-06-17 23:26:42 -0400

    Adding Static Home page

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Atonement::Application.config.secret_key_base = '288563eaa31af4222478f686bc40e9f6bcb5e60b776c05776878808893c91506ea6e35bccaa511b29d1cfe38ba2c380b837cd30024bb3afd87f986a923f45c0a'
+Atonement::Application.config.secret_token = '4ab1910323abd03fcd400b79bec7d5f2f57208c913a2acaab0bf829279faa4044e976649887d8430697b364a3b0ebb9d9fd67383d12f617ac7db3349323238e7'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
 Atonement::Application.config.session_store :cookie_store, key: '_atonement_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Atonement::Application.config.session_store :active_record_store
