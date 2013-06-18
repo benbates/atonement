f1e452e639e020e61c0b0cbf90bd5cf5d0e08832
Ben Bates <ben.bates@runkeeper.com>
2013-06-17 22:38:18 -0400

    First commit of atonement rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Atonement::Application.config.secret_key_base = '288563eaa31af4222478f686bc40e9f6bcb5e60b776c05776878808893c91506ea6e35bccaa511b29d1cfe38ba2c380b837cd30024bb3afd87f986a923f45c0a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Atonement::Application.config.session_store :cookie_store, key: '_atonement_session'
