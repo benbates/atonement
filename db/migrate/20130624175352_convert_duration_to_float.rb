c4da37c0887d5e42edef42b6c5bc2859b29aaa28
Ben Bates <ben.bates@runkeeper.com>
2013-06-24 14:14:58 -0400

    Remove duration conversion

--- db/migrate/20130624175352_convert_duration_to_float.rb
+++
@@ -1,9 +0,0 @@
-class ConvertDurationToFloat < ActiveRecord::Migration
-  def up
-    change_column :activities, :duration, :float
-  end
-
-  def down
-    change_column :activities, :duration, :decimal
-  end
-end
