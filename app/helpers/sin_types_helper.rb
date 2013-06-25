f7955e8912f62d81b0987db2ec92a4864f5f1b7c
Ben Bates <ben.bates@runkeeper.com>
2013-06-24 21:55:21 -0400

    Adding atonement and quantity to home page

--- app/helpers/sin_types_helper.rb
+++ app/helpers/sin_types_helper.rb
@@ -1,2 +1,11 @@
 module SinTypesHelper
+  def sin_type_names
+    all_sin_types = SinType.all
+    data_source = '['
+    all_sin_types.each do |st|
+      data_source << '"'+st.name+'", '
+    end
+    data_source.chomp!(", ")
+    data_source << ']'
+  end
 end
