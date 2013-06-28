9c13e9663c2da831772085fb16d2186d1ca30099
Jon Gilman <jon.gilman@gmail.com>
2013-06-28 11:22:28 -0400

    Success must work now

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def notifications
+		  if flash[:success]
+		    render :partial => 'layouts/success', :object => flash[:success]
+		  elsif flash[:notice]
+		    render :partial => 'layouts/notice', :object => flash[:notice] 
+		  elsif flash[:error]
+		    render :partial => 'layouts/error', :object => flash[:error]
+		  end
+	end
 end
